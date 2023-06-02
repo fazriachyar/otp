67f610bdb14737015aafd5f1bea33419c4c47433
fazriachyar <69377603+fazriachyar@users.noreply.github.com>
2023-06-02 16:48:13 +0700

    Add files via upload

--- createSecret.php
+++ createSecret.php
@@ -1,12 +1,24 @@
 <?php
+	session_start();
 	require_once 'C:\laragon\www\otp\vendor\autoload.php';
+	use Predis\Client;
 
 	$ga = new PHPGangsta_GoogleAuthenticator();
 	$secret = $ga->createSecret();
 	$otp = $ga->getCode($secret);
+	$_SESSION['secret'] = $secret;
 
 	echo "Secret Code : ".$secret." <br>";
 	echo "OTP Code    : ".$otp;
+	try {
+			$redis = new Client();
+			$redis->hset("user:123","secret", $secret);
+			$redis->hset("user:123","otp", $otp);
+			// $value = $redis->hgetall("user:123");
+	}
+	catch (Exception $e) {
+			die ($e->getMessage());
+	}
 ?>
 <br>
 <br>
@@ -15,13 +27,12 @@ Expierd in : <h4 id="timer"></h4>
 <script>
 	var timeLeft = 30;
 	var elem = document.getElementById('timer');
-
 	var timerId = setInterval(countdown, 1000);
 
 	function countdown() {
 		if (timeLeft == -1) {
 			clearTimeout(timerId);
-			updateOtp();
+			elem.innerHTML = 'OTP Expired';
 		} else {
 			elem.innerHTML = timeLeft + ' seconds remaining';
 			timeLeft--;
--- verifyCode.php
+++ verifyCode.php
@@ -1,6 +1,18 @@
 <?php
 	require_once 'C:\laragon\www\otp\vendor\autoload.php';
+	use Predis\Client;
 	$ga = new PHPGangsta_GoogleAuthenticator();
+
+	try {
+		$redis = new Client();
+		// $redis->hset("user:123","secret", $secret);
+		// $redis->hset("user:123","otp", $otp);
+		$value = $redis->hgetall("user:123");
+		var_dump($value);
+	}
+	catch (Exception $e) {
+			die ($e->getMessage());
+	}
 ?>
 
 <form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
@@ -11,9 +23,16 @@
 </form>
 
 <?php
-		$checkResult = $ga->verifyCode($_POST['secret'], $_POST['otp'],2);
+		// $checkResult = $ga->verifyCode($_POST['secret'], $_POST['otp'],2);
+	  // if ($checkResult) {
+		// 		echo '<h1> Success !</h1>';
+		// } else {
+		// 	echo '<h1> Failed !</h1>';
+		// }
+		$checkResult = $ga->verifyCode($value['secret'], $value['otp'],1);
 	  if ($checkResult) {
 				echo '<h1> Success !</h1>';
+				$redis->del("user:123");
 		} else {
 			echo '<h1> Failed !</h1>';
 		}
